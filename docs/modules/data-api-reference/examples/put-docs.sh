f03f29117114014b75c4b3d2fafe8194adfb41cf
Simon Dew <Simon.Dew@couchbase.com>
2023-03-22 22:20:56 +0000

    Tighten up examples and snippets

--- docs/modules/data-api-reference/examples/put-docs.sh
+++ docs/modules/data-api-reference/examples/put-docs.sh
@@ -4,7 +4,7 @@
 curl -X PUT \
      -H 'Content-Type:application/json' \
      -u "${api_key}:${secret_key}" \
-     -d '[ { "id": "x_y_z2", "value": { "name": "xyz2", "country": "US", "updated": true } },
-           { "id": "abc2", "value": { "name": "abc2", "country": "India", "updated": true } } ]' \
+     -d '[{"id": "x_y_z2", "value": {"name": "xyz2", "country": "US", "updated": true}},
+          {"id": "abc2", "value": {"name": "abc2", "country": "India", "updated": true}}]' \
 "${endpoint}/v1/scopes/samples/collections/airline/docs?pretty=true"
 # end::request[]
