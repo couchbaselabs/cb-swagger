f8c3ba72e5ce877ad9d8cd6459426b3d6ac9f6af
Simon Dew <Simon.Dew@couchbase.com>
2025-05-30 14:51:06 +0100

    More minor nits

--- docs/modules/n1ql-rest-query/examples/exsuccessful.sh
+++ docs/modules/n1ql-rest-query/examples/exsuccessful.sh
@@ -1,3 +1,4 @@
 curl -v http://localhost:8093/query/service \
-     -d 'statement=SELECT name FROM `travel-sample`.inventory.hotel LIMIT 1' \
+     -H 'Content-Type: application/json' \
+     -d '{ "statement": "SELECT name FROM `travel-sample`.inventory.hotel LIMIT 1;" }' \
      -u Administrator:password
