56cf6fc6240537b6558480f87df721e52d624d71
Simon Dew <39966290+simon-dew@users.noreply.github.com>
2025-07-09 22:35:17 +0100

    AV-102698: Add passthroughs to Data API documentation (#172)
    * Add query examples
    * Update Base URLs and credentials
    * Add additional property name
    * Minor nits
    * Build output documentation
    
    ---------
    
    Co-authored-by: tech-comm-team-couchbase <tech-comm-team@couchbase.com>

---
+++ docs/modules/n1ql-rest-query/examples/exnumbered.sh
@@ -0,0 +1,5 @@
+curl -v $BASEURL/_p/query/query/service \
+     -d 'statement=SELECT airline FROM `travel-sample`.inventory.route
+                   WHERE sourceairport = $1 AND distance > $2
+       & args=["LAX", 13000]' \
+     -u $USER:$PASSWORD
---
+++ docs/modules/n1ql-rest-query/examples/exreadonly.sh
@@ -0,0 +1,2 @@
+curl -v $BASEURL/_p/query/query/service?statement=SELECT%20name%20FROM%20%60travel-sample%60.inventory.hotel%20LIMIT%201%3B \
+     -u $USER:$PASSWORD
