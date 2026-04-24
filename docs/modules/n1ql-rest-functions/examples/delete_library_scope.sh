a25beeb3d21b76ffe182693cd5cfec504e6291e0
Simon Dew <Simon.Dew@couchbase.com>
2026-04-24 21:26:28 +0100

    Remove unnecessary Query examples

--- docs/modules/n1ql-rest-functions/examples/delete_library_scope.sh
+++
@@ -1,3 +0,0 @@
-curl -X DELETE \
-"$BASEPATH/evaluator/v1/libraries/science?bucket=travel-sample&scope=inventory" \
--u $USER:$PASSWORD
--- docs/modules/n1ql-rest-query/examples/exsuccessful.sh
+++
@@ -1,4 +0,0 @@
-curl -v $BASEPATH/query/service \
-     -H 'Content-Type: application/json' \
-     -d '{ "statement": "SELECT name FROM `travel-sample`.inventory.hotel LIMIT 1;" }' \
-     -u $USER:$PASSWORD
